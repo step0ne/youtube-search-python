a63d06a99cfe30c6826e46ff42f7a1e2e5aa3cc7
Stepan <step0ne@yandex.ru>
2025-02-13 20:25:30 +0100

    version rise up

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding = "utf-8") as fh:
 
 setuptools.setup(
     name="youtube-search-python",
-    version="1.6.15",
+    version="1.6.16",
     author="Hitesh Kumar Saini",
     license='MIT',
     author_email="saini123hitesh@gmail.com",
